98ba3a618ac621004039d66b6677218c250a13b5
Juan Ceballos <juandavi789@hotmail.com>
2025-10-25 23:27:08 -0500

    Define methods for the restConsumer to use in the useCase

--- domain/model/src/main/java/co/onclass/model/tecnologia/gateways/TecnologiaGateway.java
+++ domain/model/src/main/java/co/onclass/model/tecnologia/gateways/TecnologiaGateway.java
@@ -1,12 +1,19 @@
 package co.onclass.model.tecnologia.gateways;
 
+import co.onclass.model.paging.SortDirection;
+import co.onclass.model.tecnologia.PaginatedCapacidadIdsResponse;
 import co.onclass.model.tecnologia.Tecnologia;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public interface TecnologiaGateway {
 
     Mono<List<Tecnologia>> asignarTecnologias(Long idCapacidad, Set<Long> tecnologias);
+
+    Mono<PaginatedCapacidadIdsResponse> getCapacidadesIdsOrdenadasPorConteo(int page, int size, SortDirection direction);
+
+    Mono<Map<Long, List<Tecnologia>>> getTecnologiasPorCapacidad(List<Long> capacidades);
 }
